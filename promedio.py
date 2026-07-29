8ff2fed059f4a4ad7dbe30b9966e2fe9d482c346
JAHNCARLO7 <jahncarlo2008@gmail.com>
2026-07-29 02:09:16 +0000

    Promedio de tres numeros completados

---
+++ promedio.py
@@ -0,0 +1,7 @@
+n1=float(input("Ingrese el primer número: "))
+n2=float(input("Ingrese el segundo número: "))
+n3=float(input("Ingrese el tercer número: "))
+
+prom=(n1+n2+n3)/3
+
+print ("Tu promedio es: ", prom)
